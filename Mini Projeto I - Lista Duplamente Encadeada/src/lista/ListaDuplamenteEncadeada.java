441ea6e94a4f8dc4faf880b295214215cf579d5f
NycolasR <nycolasramon3@gmail.com>
2020-10-20 02:41:10 -0300

    Métodos first e print implementados.

--- Mini Projeto I - Lista Duplamente Encadeada/src/lista/ListaDuplamenteEncadeada.java
+++ Mini Projeto I - Lista Duplamente Encadeada/src/lista/ListaDuplamenteEncadeada.java
@@ -2,6 +2,8 @@ package lista;
 
 public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 
+	private Node initialNode;
+	
 	@Override
 	public void add(String s) throws Exception {
 		
@@ -24,31 +26,43 @@ public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 
 	@Override
 	public int index(String s) throws Exception {
-		return 0;
+		return 0; // TODO próximo a ser feito
 	}
 
 	@Override
 	public String elemen(int i) throws Exception {
-		return null;
+		return null; // TODO próximo a ser feito
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return 0; // TODO próximo a ser feito
 	}
 
 	@Override
 	public String first() {
-		return null;
+		return size() == 0 && initialNode != null ? initialNode.getDado() : null; // O primeiro dado é o dado do nó inicial 
 	}
 
 	@Override
 	public String last() {
-		return null;
+		return null; // TODO próximo a ser feito
 	}
 
 	@Override
 	public void print() {
-		
+		System.out.print("LISTA -> ");
+
+		Node aux = initialNode; // Nó auxiliar para iterar sobre a lista
+		while(aux != null) {
+			System.out.print(aux.getDado() + " -> ");
+			aux = aux.getNextNode();
+		}
+		System.out.println("NULL");
+	}
+	
+	public static void main(String[] args) {
+		ListaDuplamenteEncadeada l = new ListaDuplamenteEncadeada();
+		System.out.println(l.first());
 	}
 }
