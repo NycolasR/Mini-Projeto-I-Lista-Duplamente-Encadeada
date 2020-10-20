d80e19eb626c6b02c44fa2871b247bceef4d1654
NycolasR <nycolasramon3@gmail.com>
2020-10-20 02:02:46 -0300

    Primeiros passos.

---
+++ Mini Projeto I - Lista Duplamente Encadeada/src/lista/ListaDuplamenteEncadeada.java
@@ -0,0 +1,54 @@
+package lista;
+
+public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
+
+	@Override
+	public void add(String s) throws Exception {
+		
+	}
+
+	@Override
+	public String removeIndex(int i) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void removeElem(String s) throws Exception {
+		
+	}
+
+	@Override
+	public String previous(String s) throws Exception {
+		return null;
+	}
+
+	@Override
+	public int index(String s) throws Exception {
+		return 0;
+	}
+
+	@Override
+	public String elemen(int i) throws Exception {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public String first() {
+		return null;
+	}
+
+	@Override
+	public String last() {
+		return null;
+	}
+
+	@Override
+	public void print() {
+		
+	}
+}
