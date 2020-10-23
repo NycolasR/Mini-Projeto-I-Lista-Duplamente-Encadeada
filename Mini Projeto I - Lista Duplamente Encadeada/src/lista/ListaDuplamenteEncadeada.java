66e5a966383dfea3cedd48f550f14412fccb9085
NycolasR <nycolasramon3@gmail.com>
2020-10-23 02:04:27 -0300

    Otimização dos métodos e remoção do método isNodePresente.

--- Mini Projeto I - Lista Duplamente Encadeada/src/lista/ListaDuplamenteEncadeada.java
+++ Mini Projeto I - Lista Duplamente Encadeada/src/lista/ListaDuplamenteEncadeada.java
@@ -5,107 +5,98 @@ public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 	// Primeiro Node da lista
 	private Node initialNode;
 	
-	// A cada incremento ou decremento na lista, atualiza-se o valor deste atributo
-	private int size;
-	
-	@Override // Implementado
+	@Override
 	public void add(String s) throws Exception {
-		if(isNodePresente(s)) {
-			throw new Exception("Elemento já presente na lista");
-		}
-		
 		Node node = new Node(s);
 		Node aux = initialNode;
 		
-		if(size == 0) {	// Insersão no começo
+		if(size() == 0) { // Insersão no começo
 			initialNode = node;
 			node.setPreviousNode(null);
+			
 		} else { // Insersão nas demais posições
+			
 			while(aux.getNextNode() != null) {
+				
+				if(aux.getDado().equals(s))
+					throw new Exception("Elemento já presente na lista");
 				aux = aux.getNextNode();
 			}
 			aux.setNextNode(node);
 			node.setPreviousNode(aux);
 		}
-		size++;
 	}
 
-	@Override // Implementado
+	@Override
 	public String removeIndex(int i) throws Exception {
 		validarSize();
 		
 		Node aux = initialNode;
 		for (int j = 0; j <= i; j++) {
 			if(j == i) {
-				Node removido = aux;
-				removeElem(aux.getDado());
-				return removido.getDado();
+				excluirNode(aux);
+				return aux.getDado();
 			}
 			aux = aux.getNextNode();
 		}
 		return null;
 	}
 
-	@Override // Implementado
+	/**
+	 * Método usado para excluir um elemento da lista.
+	 * Este método está sendo usado por ambos métodos de remoção implementados
+	 * pela interface TListaMiniProjeto.
+	 * @param node node a sex excluído da lista
+	 */
+	private void excluirNode(Node node) {
+		if(node == initialNode) { // Se o elemento a ser removido for o inicial
+			initialNode = initialNode.getNextNode();
+		
+		} else if(node.getNextNode() == null) { // Se o elemento a ser removido for o último
+			node.getPreviousNode().setNextNode(null);
+			
+		} else { // Se o elemento a ser removido for um intermediário
+			node.getNextNode().setPreviousNode(node.getPreviousNode());
+			node.getPreviousNode().setNextNode(node.getNextNode());
+		}
+		
+		// Para concluir a remoção (tanto do primeiro, ultimo ou intermediário) do elemento,
+		// é conveniente fazer seus apontadores apontar para null.
+		node.setPreviousNode(null); // O elemento a ser removido perde a referência para seu antecessor
+		node.setNextNode(null); // A referência do auxiliar para seu próximo deve ser perdida
+	}
+
+	@Override
 	public void removeElem(String s) throws Exception {
 		validarSize();
-		if(!isNodePresente(s)) {
-			throw new Exception("Elemento não encontrado na lista");
-		}
 		
 		Node aux = initialNode;
 		while(aux != null) {
 			if(aux.getDado().equals(s)) {
-				if(aux == initialNode) { // Se o elemento a ser removido for o inicial
-					initialNode = initialNode.getNextNode();
-				
-				} else if(aux.getNextNode() == null) { // Se o elemento a ser removido for o último
-					aux.getPreviousNode().setNextNode(null);
-					
-				} else { // Se o elemento a ser removido for um intermediário
-					aux.getNextNode().setPreviousNode(aux.getPreviousNode());
-					aux.getPreviousNode().setNextNode(aux.getNextNode());
-				}
-				
-				// Para concluir a remoção (tanto do primeiro, ultimo ou intermediário) do elemento,
-				// é conveniente fazer seus apontadores apontar para null.
-				aux.setPreviousNode(null); // O elemento a ser removido perde a referência para seu antecessor
-				aux.setNextNode(null); // A referência do auxiliar para seu próximo deve ser perdida
-				size--; // Se um elemento for removido, o valor de size deve perder uma unidade
+				excluirNode(aux);
+				return;
 			}
 			aux = aux.getNextNode();
 		}
-	}
-	
-	/**
-	 * Este método faz uso do método index para validar a presença de um node na lista
-	 * @param s dado do node que se deseja validar a existência na lista
-	 * @return true para elemento encontrado e false caso contrário
-	 */
-	private boolean isNodePresente(String s) {
-		try {
-			validarSize();
-			index(s);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
+		// Se a execução chegou até aqui, o elemento especificado não foi encontrado
+		throw new Exception("Elemento não encontrado na lista");
 	}
 
-	@Override // Implementado
+	@Override
 	public String previous(String s) throws Exception {
 		validarSize();
 		
-		if(isNodePresente(s)) {
-			Node aux = initialNode;
+		Node aux = initialNode;
 			
-			while(aux != null) {
-				if(aux.getDado().equals(s))
-					return aux.getPreviousNode().getDado();
-				
-				aux = aux.getNextNode();
+		while(aux != null) {
+			if(aux.getDado().equals(s)) {				
+				if(aux == initialNode)
+					throw new Exception("Este é o primeiro elemento da lista, logo, ele não possui um antecessor.");
+				return aux.getPreviousNode().getDado();
 			}
+			aux = aux.getNextNode();
 		}
+		// Se a execução chegou até aqui, o elemento especificado não foi encontrado
 		throw new Exception("Elemento não encontrado na lista");
 	}
 	
@@ -115,27 +106,27 @@ public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 	 * @throws Exception quando o valor de size indicar que não há elementos na lista
 	 */
 	private void validarSize() throws Exception {
-		if(size <= 0)
+		if(size() <= 0)
 			throw new Exception("Esta lista não possui nenhum elemento");
 	}
 
-	@Override // Implementado
+	@Override
 	public int index(String s) throws Exception {
 		validarSize();
 		Node aux = initialNode;
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < size(); i++) {
 			if(aux.getDado().equals(s)) {
 				return i;
 			}
 			aux = aux.getNextNode();
 		}
-		
-		throw new Exception("Elemento não encontrado");
+		// Se a execução chegou até aqui, o elemento especificado não foi encontrado
+		throw new Exception("Elemento não encontrado na lista");
 	}
 
-	@Override // Implementado
+	@Override
 	public String elemen(int i) throws Exception {
-		if(i >= size && i < 0) {
+		if(i >= size() || i < 0) {
 			throw new Exception("Esta lista não possui este índice.");
 		}
 		
@@ -149,17 +140,25 @@ public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 		return null;
 	}
 
-	@Override // Implementado
+	@Override
 	public int size() {
+		int size = 0;
+		
+		Node aux = initialNode;
+		while(aux != null) {
+			size++;
+			aux = aux.getNextNode();
+		}
+		
 		return size;
 	}
 
-	@Override // Implementado
+	@Override
 	public String first() {
-		return size != 0 ? initialNode.getDado() : null; // O primeiro dado é o dado do nó inicial 
+		return size() != 0 ? initialNode.getDado() : null; // O primeiro dado é o dado apontado pelo nó inicial 
 	}
 
-	@Override // Implementado
+	@Override
 	public String last() {
 		if(initialNode == null) {
 			return null;
@@ -174,7 +173,7 @@ public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 		}
 	}
 
-	@Override // Implementado
+	@Override
 	public void print() {
 		System.out.print("LISTA -> ");
 
@@ -213,7 +212,7 @@ public class ListaDuplamenteEncadeada implements TListaMiniProjeto {
 		
 		encadeada.initialNode = node0;
 		encadeada.print();
-		encadeada.size = 7;
+//		encadeada.size = 7;
 		try {
 			System.out.println(encadeada.removeIndex(6));
 			encadeada.print();
